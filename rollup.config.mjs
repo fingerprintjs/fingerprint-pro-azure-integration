c7cea2dd4d0b9e68c97ceb337f7459d9636a6150
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-03-07 11:52:51 +0100

    build: rename function

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -75,6 +75,6 @@ function makeConfig(
 
 
 export default [
-  ...makeConfig('proxy/index.ts', 'fingerprintjs-pro-aws-function'),
-  ...makeConfig('management/index.ts', 'fingerprintjs-pro-aws-function-management'),
+  ...makeConfig('proxy/index.ts', 'fingerprintjs-pro-azure-function'),
+  ...makeConfig('management/index.ts', 'fingerprintjs-pro-azure-function-management'),
 ]
