73c1bb2cef0733abe979f312a68cc1e39f237987
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-03-07 09:57:32 +0100

    chore: initial commit

---
+++ functions/proxy/index.ts
@@ -0,0 +1,33 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+
+const httpTrigger: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
+  const path = req.params?.restOfPath
+
+  switch (path) {
+    case 'client': {
+      // context.res = await downloadAgent(req);
+
+      break
+    }
+
+    case 'visitorId':
+      // context.res = await handleIngress(req);
+
+      break
+
+    default:
+      context.res = {
+        status: 404,
+        body: JSON.stringify({
+          req,
+          message: 'Invalid route',
+          restOfPath: path,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+  }
+}
+
+export default httpTrigger
---
+++ rollup.config.mjs
@@ -0,0 +1,80 @@
+import typescript from '@rollup/plugin-typescript'
+import jsonPlugin from '@rollup/plugin-json'
+import licensePlugin from 'rollup-plugin-license'
+import dtsPlugin from 'rollup-plugin-dts'
+import replace from '@rollup/plugin-replace'
+import { join } from 'path'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import dotenv from 'dotenv'
+import packageJson from './package.json' assert { type: 'json' }
+
+dotenv.config()
+const outputDirectory = 'dist'
+
+function makeConfig(
+  entryFile,
+  artifactName,
+) {
+
+  const commonBanner = licensePlugin({
+    banner: {
+      content: {
+        file: join('assets', 'license_banner.txt'),
+      },
+    },
+  })
+
+  /**
+   * @type {import('rollup').RollupOptions}
+   * */
+  const commonInput = {
+    input: 'functions/proxy/index.ts',
+    external: ['https'],
+    plugins: [
+      jsonPlugin(),
+      typescript(),
+      nodeResolve({ preferBuiltins: false }),
+      commonjs(),
+      replace({
+        __FPCDN__: process.env.FPCDN,
+        __INGRESS_API__: process.env.INGRESS_API,
+        __lambda_func_version__: packageJson.version,
+        preventAssignment: true,
+      }),
+      commonBanner,
+    ],
+  }
+
+  const commonOutput = {
+    exports: 'named',
+  }
+
+  return [
+    {
+      ...commonInput,
+      output: [
+        {
+          ...commonOutput,
+          file: `${outputDirectory}/${artifactName}.js`,
+          format: 'cjs',
+        },
+      ],
+    },
+    {
+      ...commonInput,
+      plugins: [dtsPlugin(), commonBanner],
+      output: {
+        file: `${outputDirectory}/${artifactName}.d.ts`,
+        format: 'es',
+      },
+    },
+  ]
+
+}
+
+
+export default [
+  ...makeConfig('functions/proxy/index.ts', 'fingerprintjs-pro-aws-function'),
+  ...makeConfig('functions/management/index.ts', 'fingerprintjs-pro-aws-function-management'),
+]
