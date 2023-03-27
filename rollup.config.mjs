86a6fe68b5e4d06c2b37eb0b0070ec0cd8ef5d81
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-03-27 14:14:27 +0200

    build: improve build logs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -13,7 +13,9 @@ import packageJson from './package.json' assert { type: 'json' }
 dotenv.config()
 const outputDirectory = 'dist'
 
-function makeConfig(entryFile, artifactName, functionJsonPath) {
+function makeConfig(opts, entryFile, artifactName, functionJsonPath, transformFunctionJson) {
+  const isDev = opts.watch
+
   const commonBanner = licensePlugin({
     banner: {
       content: {
@@ -37,7 +39,15 @@ function makeConfig(entryFile, artifactName, functionJsonPath) {
             transform: (contents) => {
               const json = JSON.parse(contents.toString())
 
+              if (json.disabled) {
+                console.warn(
+                  `Function ${artifactName} is disabled. To enable it, set "disabled" to false in ${functionJsonPath}.`,
+                )
+              }
+
               json.scriptFile = `./${artifactName}.js`
+
+              transformFunctionJson?.(json, isDev)
 
               return JSON.stringify(json, null, 2)
             },
@@ -93,12 +103,28 @@ function makeConfig(entryFile, artifactName, functionJsonPath) {
   ]
 }
 
-/**
- * @type {import('rollup').RollupOptions[]}
- * */
-const rollupConfig = [
-  ...makeConfig('proxy/index.ts', 'fingerprintjs-pro-azure-function', 'proxy/function.json'),
-  ...makeConfig('management/index.ts', 'fingerprintjs-pro-azure-function-management', 'management/function.json'),
-]
+export default (opts) => {
+  /**
+   * @type {import('rollup').RollupOptions[]}
+   * */
+  return [
+    ...makeConfig(opts, 'proxy/index.ts', 'fingerprintjs-pro-azure-function', 'proxy/function.json'),
+    ...makeConfig(
+      opts,
+      'management/index.ts',
+      'fingerprintjs-pro-azure-function-management',
+      'management/function.json',
+      (config, isDev) => {
+        if (isDev) {
+          if (typeof config.bindings[0].runOnStartup === 'undefined') {
+            console.info(
+              `Running management function on startup. To disable it, set "runOnStartup" to false in management/function.json.`,
+            )
 
-export default rollupConfig
+            config.bindings[0].runOnStartup = true
+          }
+        }
+      },
+    ),
+  ]
+}
