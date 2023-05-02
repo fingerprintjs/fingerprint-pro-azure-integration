7dc285fb3956b5f55a18c68f9a04f96e826f64c2
Sergey Shelomentsev <sergey.shelomentsev@fingerprint.com>
2023-05-02 21:03:25 +0300

    tests: add test for integration error

---
+++ proxy/errors/integrarionError.test.ts
@@ -0,0 +1,16 @@
+import { IntegrationError } from './IntegrationError'
+
+describe('check integration error result', () => {
+    it('check if error return valid body', async () => {
+        const json = {
+            vendor: 'Fingerprint Pro Azure Function',
+            message: 'Download failed',
+            path: 'fpjs/agent',
+        }
+        
+        const message: IntegrationError = new IntegrationError(json.message, json.path)
+        const body = message.toBody()
+    
+        expect(body).toBe(JSON.stringify(json))
+    })    
+})
