9e0aae6750902341bab8250d8883e67d2e8f2d8a
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-02-29 19:09:34 +0800

    ci: use function app host

--- e2e/scripts/mockTests.ts
+++ e2e/scripts/mockTests.ts
@@ -8,14 +8,15 @@ async function main() {
   for (const info of testInfo) {
     const agentPath = `${info.routePrefix}/${info.agentDownloadPath}`
     const resultPath = `${info.routePrefix}/${info.getResultPath}`
+    const host = info.functionAppUrl
 
-    console.info('Running mock server for', info.frontdoorUrl)
+    console.info('Running mock server for', host)
     console.info('Agent download path:', agentPath)
     console.info('Get result path:', resultPath)
 
     try {
       execSync(
-        `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${agentPath}" --ingress-proxy-path="${resultPath}"`,
+        `npm exec -y "git+https://github.com/fingerprintjs/dx-team-mock-for-proxy-integrations-e2e-tests.git" -- --host="${host}" --cdn-proxy-path="${agentPath}" --ingress-proxy-path="${resultPath}"`,
         {
           stdio: 'inherit',
         },
