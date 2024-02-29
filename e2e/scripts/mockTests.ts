f9ec3ff8eea7b575e80290b911ffeace19a0f1e8
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-02-29 14:40:58 +0800

    test: add more logs

--- e2e/scripts/mockTests.ts
+++ e2e/scripts/mockTests.ts
@@ -4,6 +4,10 @@ import { readTestInfo } from '../shared/testInfo'
 async function main() {
   const testInfo = readTestInfo()
   for (const info of testInfo) {
+    console.info('Running mock server for', info.frontdoorUrl)
+    console.info('Agent download path:', info.agentDownloadPath)
+    console.info('Get result path:', info.getResultPath)
+
     execSync(
       `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${info.agentDownloadPath}" --ingress-proxy-path="${info.getResultPath}"`,
       {
