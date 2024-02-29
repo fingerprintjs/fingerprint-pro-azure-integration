7b7c9891969adbdaeaf73f7d3bf189c4444a4898
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-02-29 18:32:24 +0800

    test: remove retry logic

--- e2e/scripts/mockTests.ts
+++ e2e/scripts/mockTests.ts
@@ -1,17 +1,11 @@
 import { execSync } from 'child_process'
 import { readTestInfo } from '../shared/testInfo'
-import { ExponentialBackoff, handleAll, retry } from 'cockatiel'
 
 async function main() {
   let hasError = false
   const testInfo = readTestInfo()
 
   for (const info of testInfo) {
-    const policy = retry(handleAll, {
-      maxAttempts: 3,
-      backoff: new ExponentialBackoff(),
-    })
-
     const agentPath = `${info.routePrefix}/${info.agentDownloadPath}`
     const resultPath = `${info.routePrefix}/${info.getResultPath}`
 
@@ -20,13 +14,11 @@ async function main() {
     console.info('Get result path:', resultPath)
 
     try {
-      await policy.execute(() =>
-        execSync(
-          `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${agentPath}" --ingress-proxy-path="${resultPath}"`,
-          {
-            stdio: 'inherit',
-          },
-        ),
+      execSync(
+        `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${agentPath}" --ingress-proxy-path="${resultPath}"`,
+        {
+          stdio: 'inherit',
+        },
       )
     } catch (e) {
       console.error(e)
