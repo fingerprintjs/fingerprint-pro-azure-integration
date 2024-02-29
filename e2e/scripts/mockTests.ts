2cf4382c01b4e085db601eef346d3418e42e4340
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-02-29 17:24:59 +0800

    test: run all tests before failing

--- e2e/scripts/mockTests.ts
+++ e2e/scripts/mockTests.ts
@@ -2,6 +2,8 @@ import { execSync } from 'child_process'
 import { readTestInfo } from '../shared/testInfo'
 
 async function main() {
+  let hasError = false
+
   const testInfo = readTestInfo()
   for (const info of testInfo) {
     const agentPath = `${info.routePrefix}/${info.agentDownloadPath}`
@@ -11,12 +13,22 @@ async function main() {
     console.info('Agent download path:', agentPath)
     console.info('Get result path:', resultPath)
 
-    execSync(
-      `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${agentPath}" --ingress-proxy-path="${resultPath}"`,
-      {
-        stdio: 'inherit',
-      },
-    )
+    try {
+      execSync(
+        `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${agentPath}" --ingress-proxy-path="${resultPath}"`,
+        {
+          stdio: 'inherit',
+        },
+      )
+    } catch (e) {
+      console.error(e)
+
+      hasError = true
+    }
+  }
+
+  if (hasError) {
+    process.exit(1)
   }
 }
 
