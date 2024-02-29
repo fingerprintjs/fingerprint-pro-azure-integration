9d91635efd124f1fb2398e57c13541e897541dad
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-02-29 16:25:55 +0800

    test: use correct paths

--- e2e/scripts/mockTests.ts
+++ e2e/scripts/mockTests.ts
@@ -4,12 +4,15 @@ import { readTestInfo } from '../shared/testInfo'
 async function main() {
   const testInfo = readTestInfo()
   for (const info of testInfo) {
+    const agentPath = `${info.routePrefix}/${info.agentDownloadPath}`
+    const resultPath = `${info.routePrefix}/${info.getResultPath}`
+
     console.info('Running mock server for', info.frontdoorUrl)
-    console.info('Agent download path:', info.agentDownloadPath)
-    console.info('Get result path:', info.getResultPath)
+    console.info('Agent download path:', agentPath)
+    console.info('Get result path:', resultPath)
 
     execSync(
-      `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${info.agentDownloadPath}" --ingress-proxy-path="${info.getResultPath}"`,
+      `npm exec -y "git+https://github.com/fingerprintjs/mock-for-e2e.git" -- --host="${info.frontdoorUrl}" --cdn-proxy-path="${agentPath}" --ingress-proxy-path="${resultPath}"`,
       {
         stdio: 'inherit',
       },
