1c30179bfd9b8322b0f3131fcf989296befc81ae
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-05-19 11:33:47 +0200

    refactor: enhance logging and error handling in package backup operations

--- management/storage.ts
+++ management/storage.ts
@@ -3,11 +3,12 @@ import { InvocationContext } from '@azure/functions'
 import { BACKUP_PACKAGE_BLOB, RELEASED_PACKAGE_BLOB } from './settings'
 
 export async function createPackageBackup(containerClient: ContainerClient, logger?: InvocationContext) {
-  logger?.debug('Creating backup of current released package')
   const sourceClient = containerClient.getBlockBlobClient(RELEASED_PACKAGE_BLOB)
   const destClient = containerClient.getBlockBlobClient(BACKUP_PACKAGE_BLOB)
+  logger?.debug('Creating backup of current released package', sourceClient.url, '->', destClient.url)
   const poller = await destClient.beginCopyFromURL(sourceClient.url)
   await poller.pollUntilDone()
+  logger?.debug('Backup created', BACKUP_PACKAGE_BLOB)
 }
 
 export async function restorePackageFromBackup(containerClient: ContainerClient, logger?: InvocationContext) {
@@ -19,6 +20,10 @@ export async function restorePackageFromBackup(containerClient: ContainerClient,
 }
 
 export async function deletePackageBackup(containerClient: ContainerClient, logger?: InvocationContext) {
-  logger?.debug('Deleting package backup')
-  await containerClient.deleteBlob(BACKUP_PACKAGE_BLOB)
+  logger?.debug('Deleting package backup', BACKUP_PACKAGE_BLOB)
+  try {
+    await containerClient.deleteBlob(BACKUP_PACKAGE_BLOB)
+  } catch (e) {
+    logger?.error('Failed to delete package backup', e)
+  }
 }
