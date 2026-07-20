4aa84235ec1d65194c5719e0119d7e26b1f00035
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-07-20 10:02:01 +0200

    chore: exclude PUT and HEAD requests from mock tests in e2e script

--- e2e/scripts/mockTests.ts
+++ e2e/scripts/mockTests.ts
@@ -30,6 +30,7 @@ async function doMockTests() {
       'traffic-name': 'fingerprint-pro-azure',
       'integration-version': pkg.version,
       'enable-new-tests': 'true',
+      exclude: ['PUT request', 'HEAD request'],
     } as Record<string, string | string[]>
 
     console.info('Running mock tests with args:', args)
