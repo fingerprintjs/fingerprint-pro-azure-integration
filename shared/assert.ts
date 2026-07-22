b393612d70c4c98ed6debfb581074ff89229899f
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-07-22 15:00:59 +0200

    refactor: update `assertIsTruthy` and `isTruthy` to use `FalseValue` type

--- shared/assert.ts
+++ shared/assert.ts
@@ -1,9 +1,11 @@
-export function assertIsTruthy<T>(value: T | null | undefined | '', message?: string): asserts value is T {
+export function assertIsTruthy<T>(value: T | FalseValue, message?: string): asserts value is T {
   if (!isTruthy(value)) {
     throw new Error(message ?? 'Expected value to be truthy')
   }
 }
 
-export function isTruthy<T>(value: T | null | undefined | ''): value is T {
+export type FalseValue = false | 0 | null | undefined
+
+export function isTruthy<T>(value: T | FalseValue): value is T {
   return Boolean(value)
 }
